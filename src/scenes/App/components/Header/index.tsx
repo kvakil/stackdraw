0d1461f38e354a7d71ca1a356557aaf9c59d8033
Keyhan Vakil <kvakil@berkeley.edu>
2017-10-07 14:57:51 -0700

    Finish header bar

--- src/scenes/App/components/Header/index.tsx
+++ src/scenes/App/components/Header/index.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { Link } from 'react-router-dom';
 
 class Header extends React.Component {
   render() {
@@ -8,6 +9,26 @@ class Header extends React.Component {
           <div className="navbar-brand">
             <div className="navbar-item">
               StackDraw (logo here)
+            </div>
+          </div>
+
+          <div className="navbar-menu">
+            <div className="navbar-start">
+              <Link to="/" className="navbar-item">
+                Home
+              </Link>
+              <Link to="/main" className="navbar-item">
+                Drawer
+              </Link>
+            </div>
+            <div className="navbar-end">
+              <div className="navbar-item">
+                <p className="control">
+                  <a className="button is-primary">
+                    View on Github
+                  </a>
+                </p>
+              </div>
             </div>
           </div>
         </nav>
