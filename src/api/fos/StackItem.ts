19a4bdad895c0de4d8691deda0fdcabe7c18f5ae
Keyhan Vakil <kvakil@berkeley.edu>
2017-10-09 23:40:19 -0700

    Add another newline for spacing

--- src/api/fos/StackItem.ts
+++ src/api/fos/StackItem.ts
@@ -3,6 +3,7 @@ import FrameObject from '../FrameObject';
 export default class StackItem implements FrameObject {
     location: number;
     label: string;
+
     constructor(location: number, label: string) {
         this.location = location;
         this.label = label;
