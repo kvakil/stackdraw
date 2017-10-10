25a09ceeb7577994e47a8071646d3a034e6b0cba
Keyhan Vakil <kvakil@berkeley.edu>
2017-10-09 23:41:10 -0700

    Add constructor for pointer

--- src/api/fos/Pointer.ts
+++ src/api/fos/Pointer.ts
@@ -4,4 +4,10 @@ export default class Pointer implements FrameObject {
     location: number;
     label: string;
     to: number;
+
+    constructor(location: number, label: string, to: number) {
+        this.location = location;
+        this.label = label;
+        this.to = to;
+    }
 }
