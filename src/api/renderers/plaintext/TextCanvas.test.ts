1a1e83fe4703880b82e7ba63f31a3b46d69313ee
Keyhan Vakil <kvakil@berkeley.edu>
2017-10-14 11:22:02 -0700

    Create tests for the TextCanvas

--- src/api/renderers/plaintext/TextCanvas.test.ts
+++ src/api/renderers/plaintext/TextCanvas.test.ts
@@ -4,3 +4,44 @@ it('can be initialized and exported', () => {
     const tc = new TextCanvas();
     expect(tc.toString()).toBe('');
 });
+
+it('correctly sets individual characters', () => {
+    const tc = new TextCanvas();
+    tc.set(0, 0, 'a');
+    expect(tc.toString()).toBe('a');
+});
+
+it('appropriately expands the canvas', () => {
+    const tc = new TextCanvas();
+    tc.set(2, 3, 'a');
+    const expected = [
+        '    ',
+        '    ',
+        '   a'
+    ].join('\n');
+    expect(tc.toString()).toBe(expected);
+});
+
+it('allows multiple sets', () => {
+    const tc = new TextCanvas();
+    tc.set(2, 3, 'a');
+    tc.set(1, 1, 'b');
+    tc.set(0, 5, 'c');
+    const expected = [
+        '     c',
+        ' b    ',
+        '   a  '
+    ].join('\n');
+    expect(tc.toString()).toBe(expected);
+});
+
+it('allows writing strings to locations', () => {
+    const tc = new TextCanvas();
+    tc.text(2, 3, 'abc');
+    const expected = [
+        '      ',
+        '      ',
+        '   abc'
+    ].join('\n');
+    expect(tc.toString()).toBe(expected);
+});
