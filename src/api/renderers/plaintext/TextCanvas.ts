9b4e40b563a7aa85be6dbd6949bfb81cbebcec1c
Keyhan Vakil <kvakil@berkeley.edu>
2017-10-14 11:23:46 -0700

    Add functions for writing characters and strings to canvas

--- src/api/renderers/plaintext/TextCanvas.ts
+++ src/api/renderers/plaintext/TextCanvas.ts
@@ -1,3 +1,5 @@
+import 'core-js/es7/string';
+
 /**
  * A canvas which can be drawn on and exports to ASCII.
  * Its coordinates are labelled starting from (0, 0) at the top-left,
@@ -23,5 +25,49 @@ export default class TextCanvas {
      */
     toString(): string {
         return this.grid.join('\n');
+    }
+
+    /**
+     * Adds a character to the given location, overwriting any old ones.
+     * 
+     * It is undefined behavior if this is called with `char` having
+     * length greater than 1.
+     * 
+     * @param row the row of the new character.
+     * @param col the column of the new character.
+     * @param char a string containing only one character.
+     */
+    set(row: number, col: number, char: string) {
+        this.text(row, col, char);
+    }
+
+    /**
+     * Writes a string starting at the given row and column.
+     * 
+     * @param row the row to start writing at.
+     * @param col the column to start writing at.
+     * @param toWrite the string to write.
+     */
+    text(row: number, col: number, toWrite: string) {
+        const maxColumn = col + toWrite.length - 1;
+        this.expand(row, maxColumn);
+        const rowToChange = this.grid[row];
+        const left = rowToChange.slice(0, col);
+        const right = rowToChange.slice(maxColumn + 1);
+        this.grid[row] = left + toWrite + right;
+    }
+
+    /**
+     * Expands the canvas to the given dimensions.
+     * 
+     * @param row the new maximum row.
+     * @param col the new maximum column.
+     */
+    private expand(row: number, col: number) {
+        while (this.grid.length <= row) {
+            this.grid.push('');
+        }
+
+        this.grid = this.grid.map(line => line.padEnd(col + 1, ' '));
     }
 }
