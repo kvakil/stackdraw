d658f36899cc2ab86a5475ca8fac58ad157b88c4
Keyhan Vakil <kvakil@berkeley.edu>
2017-10-21 12:37:03 -0700

    Lift state up into View

--- src/scenes/Main/components/View/index.tsx
+++ src/scenes/Main/components/View/index.tsx
@@ -1,13 +1,22 @@
 import * as React from 'react';
-import ViewControl from './components/ViewControl';
-import ViewCanvas from './components/ViewCanvas';
 
 export default class View extends React.Component {
   render() {
     return (
       <div className="View">
-        <ViewControl />
-        <ViewCanvas />
+        <div className="ViewControl field is-grouped is-grouped-centered">
+        <p className="control is-primary">
+          <input className="button" type="button" value="Next" />
+        </p>
+        <p className="control">
+          <input className="button" type="button" value="Prev" />
+        </p>
+      </div>
+      <div className="ViewCanvas">
+          <code id="canvas">
+            &nbsp;
+          </code>
+        </div>
       </div>
     );
   }
