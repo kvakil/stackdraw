6f503adf09f712371e4372aba666ce0689ea4096
Keyhan Vakil <kvakil@berkeley.edu>
2017-10-21 12:37:19 -0700

    Add JSDoc comment to interpret

--- src/api/Interpret.ts
+++ src/api/Interpret.ts
@@ -1,6 +1,13 @@
 import plaintextExport from './renderers/plaintext/Plaintext';
 import evaluate from './dsl/Evaluate';
 
+/**
+ * Attempts to render the DSL code into a plaintext stack drawing.
+ * 
+ * @param code the DSL code which should be parsed
+ * @return {string[]} a list of strings, each of which contains one frame
+ * @return {Error[]} a list of errors which occurred
+ */
 export function plaintext(code: string): {text: string[], errors: Error[]} {
     const {frames, errors: evalErrors} = evaluate(code);
     if (evalErrors.length > 0) { return {text: [], errors: evalErrors}; }
