2400e3ef3bdefbc8725ae1550c76d24bcc523153
Keyhan Vakil <kvakil@berkeley.edu>
2017-10-21 12:41:44 -0700

    Move Render button to the Editor pane

--- src/scenes/Main/components/Editor/index.tsx
+++ src/scenes/Main/components/Editor/index.tsx
@@ -5,6 +5,11 @@ export default class Editor extends React.Component {
   render() {
     return (
       <div className="Editor">
+        <div className="field is-grouped is-grouped-right">
+          <p className="control">
+            <input className="button is-primary" type="button" value="Render" />
+          </p>
+        </div>
         <textarea />
       </div>
     );
