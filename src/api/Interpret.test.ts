056746ce54770fbe96ebe3b61626326410b817c4
Keyhan Vakil <kvakil@berkeley.edu>
2017-10-20 17:44:26 -0700

    Add test for multiple stack frames

--- src/api/Interpret.test.ts
+++ src/api/Interpret.test.ts
@@ -17,3 +17,33 @@ it('interprets simple stack frames', () => {
     expect(text).toHaveLength(1);
     expect(text[0]).toBe(expectedResult);
 });
+
+it('interprets multiple stack frames', () => {
+    const {text, errors} = plaintext([
+        'frame',
+        'push label=eax',
+        'push label=ebx',
+        'frame',
+        'push label=ecx'
+    ].join('\n'));
+    expect(errors).toHaveLength(0);
+    const expectedResult1 = [
+        '       ',
+        '┌─────┐',
+        '│ eax │',
+        '│ ebx │',
+        '│     │',
+        '└─────┘'
+    ].join('\n');
+    const expectedResult2 = [
+        '       ',
+        '┌─────┐',
+        '│ eax │',
+        '│ ebx │',
+        '│ ecx │',
+        '└─────┘'
+    ].join('\n');
+    expect(text).toHaveLength(2);
+    expect(text[0]).toBe(expectedResult1);
+    expect(text[1]).toBe(expectedResult2);
+});
